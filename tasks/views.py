a9c611460e8b4bd4c0263666dfcf85c7c936685b
Eirieel <aespinozac@usm.cl>
2023-10-22 23:20:13 -0300

    Login

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -34,7 +34,17 @@ def cerrar_sesion(request):
     return redirect('home')
 
 def login_view(request):
-    return render(request, 'login_view.html')#{'form': User} buscar si es que existe un formulario para login precreado por django
+    if request.method == 'GET':         
+        return render(request, 'login_view.html', {'form' : AuthenticationForm})
+    else:
+        user=authenticate
+        authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'login_view.html', {'form' : AuthenticationForm, 'error':'El User o contraseña son incorrectos'})
+        else:
+            login(request, user)
+            return redirect('home')
+        
 
 def nosotros(request):
     return render(request,'about-us.html')#hacer el html que contenga la informacion sobre nosotros
