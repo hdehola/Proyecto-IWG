4106024579ee714e0d834d1cc7aceec6220eae0f
Hans <gonzalezhans79u@example.com>
2023-11-08 10:21:37 -0300

    api

--- pruebas/funcion api.py
+++ pruebas/funcion api.py
@@ -7,6 +7,26 @@ payload={}
 headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 alo = response.json()
+if alo ["status"] == "success":   #Cambiar los print por return cuando se agregue a la función#
+    dato = alo["data"]["current"]["pollution"]["aqius"]
+    print(dato)
+    if dato >= 0 and dato <= 50:
+        print(str(dato)+":", "Verde")
+    if dato > 50 and dato <= 100:
+        print(str(dato)+":","Amarillo")
+    if dato > 100 and dato <= 150:
+        print(str(dato)+":","Naranja")
+    if dato > 150 and dato <= 200:
+        print(str(dato)+":","Rojo")
+    if dato > 200 and dato <= 300:
+        print(str(dato)+":","Morado")
+    if dato > 300:
+        print(str(dato)+":","Cafe")
+else:
+    print("Zona no encontrada")
+
+#otros datos#
+'''
 if alo ["status"] == "success":
     aqius = alo["data"]["current"]["pollution"]["aqius"]
     mainus = alo["data"]["current"]["pollution"]["mainus"]
@@ -18,3 +38,4 @@ if alo ["status"] == "success":
     print(maincn)
 else:
     print("Zona no encontrada")
+'''
