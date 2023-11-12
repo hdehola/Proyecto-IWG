438eea73db935173f6c80c6e5494f9df6dc6299d
Eirieel <aespinozac@usm.cl>
2023-11-12 06:47:42 -0300

    API EN HTML

--- pruebas/funcion api.py
+++ pruebas/funcion api.py
@@ -1,6 +1,8 @@
 import requests
 estado = "Valparaiso" #variable que tiene que ingresar el usuario region
+estado = estado.replace(' ', '%20')
 ciudad = "Valparaiso" #variable que tiene que ingresar el usuario comuna
+ciudad = ciudad.replace(' ', '%20')
 url = ["http://api.airvisual.com/v2/city?city="+ciudad+"&state="+estado+"&country=Chile&key=4217e686-4099-4071-b670-5664769faaad"]
 url = "".join(url)
 payload={}
